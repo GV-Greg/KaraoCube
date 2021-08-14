efaecf26dc6ee477e89daabed1bfc6575744e886
Greg GV <gregvandenbergh@gmail.com>
2021-08-14 02:38:34 +0200

    Purge CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 const colors = require('tailwindcss/colors');
 
 module.exports = {
-    purge: [
-        './resources/**/*.blade.php',
-        './resources/**/*.js',
-        './resources/**/*.vue',
-    ],
+    purge: {
+        enabled: process.env.PURGE_CSS === 'production',
+        content: [
+            './resources/**/*.blade.php',
+            './resources/**/*.js',
+            './resources/**/*.vue',
+        ]
+    },
     darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {},
